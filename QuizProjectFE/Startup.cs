13bcf108a972bdd7b59f429df57071593b3f3263
Jordan Lapardin <jordanlapa@outlook.com>
2022-05-04 16:25:46 +1000

    Maybe Now

--- QuizProjectFE/Startup.cs
+++ QuizProjectFE/Startup.cs
@@ -70,7 +70,7 @@ namespace QuizProjectFE
             {
                 //app.UseExceptionHandler("/Home/Error");
                 //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
